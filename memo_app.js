3aa78cdb705c117781de3ee91999dd7798700771
Seokha Ko <sh.ko@lge.com>
2017-04-03 06:50:30 -0400

    separate js and html.

---
+++ memo_app.js
@@ -0,0 +1,28 @@
+var memoList = [];
+var memo;
+
+window.onload = function(){
+	memo = document.getElementById("memo");
+
+	var save_button = document.getElementById('save');
+	save_button.addEventListener('click', save_click);
+}
+
+function getMemoTexts(){
+	var result = "";
+	for(var i=0; i < memoList.length; i ++){
+		// console.log(memoList[i].text);
+		// console.log(memoList[i].date);
+		result = result + memoList[i].date.toString() + " " + memoList[i].text + "\n";
+	}
+	return result;
+}
+
+function save_click() {
+	var memoObj = new Object();
+	memoObj.date = new Date();
+	memoObj.text = memo.value;
+	memoList.push(memoObj);
+	memo.value = "";
+	alert(getMemoTexts());
+}
